b26ef8e30d308cd472f17e72b11d5ae6019963d7
gui-rossi <guizo.rossi@gmail.com>
2021-02-16 20:54:59 -0300

    Added first audio ratinhooo

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import BackgroundImage from './src/components/BackgroundImage/BackgroundImage';
 import Button from './src/components/Button/Button';
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  const [sound, setSound] = useState();
+
+  async function playRatinhooo() {
+    console.warn('funfou');
+    const { sound } = await Audio.Sound.createAsync(
+       require('./audio/ratinhooo.mp3')
+    );
+    setSound(sound);
+  
+    await sound.playAsync(); 
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={style.container}>
       
       <BackgroundImage />
 
       <Button
-        text="Uepá"
+        text="Ratinhoooo"
+        onPress={playRatinhooo}
       />
       
       <StatusBar style="auto" />
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { View, Text, Pressable } from 'react-native';
 import ButtonStyles from './ButtonStyles';
 
-function saiSom () {
-    console.warn('alo');
-}
-
 function Button(props) {
     return (
         <View style={ButtonStyles.container}>
-            <Pressable style={ButtonStyles.button} onPress={() => saiSom()}>
+            <Pressable style={ButtonStyles.button} onPress={props.onPress}>
                 <Text style={ButtonStyles.text}>{props.text}</Text>
             </Pressable>
         </View>
